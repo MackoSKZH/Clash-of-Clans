83bdf1442ce403d4f4d6ed16b3bed891514cf7e1
Martin <martin.hronsky.007@gmail.com>
2025-05-28 20:46:02 +0200

    Deleted unnecessary comments in map.

--- src/mapa/Mapa.java
+++ src/mapa/Mapa.java
@@ -6,13 +6,11 @@ import budovy.TovarenBudov;
 import main.Hra;
 import obraz.HernePole;
 
-//Trieda reprezentujúca hernú mapu, ktorá obsahuje dvojrozmerné pole segmentov a referenciu na hru.
 public class Mapa {
     private final int pocetSegmentov;
     private final Segment[][] segmenty;
     private final Hra hra;
 
-    //Konštruktor mapy. Inicializuje mriežku segmentov podľa zadaného počtu a priradí odkaz na hru.
     public Mapa(Hra hra, int pocet) {
         this.hra = hra;
         this.pocetSegmentov = pocet;
@@ -24,7 +22,6 @@ public class Mapa {
         }
     }
 
-    //Nastaví predvolené budovy do stredových segmentov mapy (radnica, dom staviteľa, zlatý dôl, kanón).
     public void nastavSegmenty() {
         int stred = this.pocetSegmentov / 2;
 
@@ -34,22 +31,18 @@ public class Mapa {
         this.segmenty[stred][stred - 1].setBudovy(TovarenBudov.vytvorPodlaTypu(TypBudovy.KANON, 1, TypBudovy.KANON.getZivoty()));
     }
 
-    //Vracia segment na daných súradniciach mapy.
     public Segment getSegment(int x, int y) {
         return this.segmenty[x][y];
     }
 
-    //Vracia počet segmentov mapy v jednom rozmere (mapa je štvorcová).
     public int getPocetSegmentov() {
         return this.pocetSegmentov;
     }
 
-    //Vracia referenciu na aktuálnu inštanciu hry.
     public Hra getHra() {
         return this.hra;
     }
 
-    //Vracia herné pole z obrazovky na základe pozície segmentu v mape.
     public HernePole getHernePole(int riadok, int stlpec) {
         return this.getHra().getObrazovka().getHernePole(riadok, stlpec);
     }
